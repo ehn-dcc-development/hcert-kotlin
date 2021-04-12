7212a0ce7512956663ff1feadb6bc4bd6b939f5b
Christian Kollmann <christian.kollmann@a-sit.at>
2021-04-12 14:18:35 +0200

    Wrap HCERT inside a CBOR map to conform to spec

--- src/main/kotlin/ehn/techiop/hcert/kotlin/chain/CborService.kt
+++ src/main/kotlin/ehn/techiop/hcert/kotlin/chain/CborService.kt
@@ -4,15 +4,19 @@ import COSE.Attribute
 import COSE.HeaderKeys
 import COSE.MessageTag
 import COSE.Sign1Message
+import com.upokecenter.cbor.CBORObject
+import ehn.techiop.hcert.kotlin.cwt.CwtHeaderKeys
 import kotlinx.serialization.cbor.Cbor
 import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.encodeToByteArray
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 
 class CborService(private val cryptoService: CryptoService) {
 
     fun sign(input: ByteArray): ByteArray {
         return Sign1Message().also {
-            it.SetContent(input)
+            it.SetContent(wrapPayload(input))
             for (header in cryptoService.getCborHeaders()) {
                 it.addAttribute(header.first, header.second, Attribute.PROTECTED)
             }
@@ -25,15 +29,41 @@ class CborService(private val cryptoService: CryptoService) {
         val kid = decoded.protectedAttributes.get(HeaderKeys.KID.AsCBOR()).AsString()
         if (!decoded.validate(cryptoService.getCborVerificationKey(kid)))
             throw IllegalArgumentException("Not validated")
-        return decoded.GetContent()
+        return unwrapPayload(decoded.GetContent())
     }
 
     inline fun <reified T> encode(input: T): ByteArray {
-        return Cbor{ ignoreUnknownKeys = true }.encodeToByteArray(input)
+        return Cbor { ignoreUnknownKeys = true }.encodeToByteArray(input)
     }
 
     inline fun <reified T> decode(input: ByteArray): T {
-        return Cbor{ ignoreUnknownKeys = true }.decodeFromByteArray<T>(input)
+        return Cbor { ignoreUnknownKeys = true }.decodeFromByteArray<T>(input)
+    }
+
+    private fun wrapPayload(input: ByteArray): ByteArray? {
+        val issueTime = Instant.now()
+        val expirationTime = issueTime.plus(365, ChronoUnit.DAYS)
+        return CBORObject.NewMap().also { payload ->
+            payload[CwtHeaderKeys.ISSUER.AsCBOR()] = CBORObject.FromObject("AT")
+            payload[CwtHeaderKeys.ISSUED_AT.AsCBOR()] = CBORObject.FromObject(issueTime.epochSecond)
+            payload[CwtHeaderKeys.EXPIRATION.AsCBOR()] = CBORObject.FromObject(expirationTime.epochSecond)
+            payload[CwtHeaderKeys.HCERT.AsCBOR()] = CBORObject.NewMap().also {
+                it[CBORObject.FromObject(1)] = CBORObject.FromObject(input)
+            }
+        }.EncodeToBytes()
+    }
+
+
+    private fun unwrapPayload(input: ByteArray): ByteArray {
+        val map = CBORObject.DecodeFromBytes(input)
+        val issuer = map[CwtHeaderKeys.ISSUER.AsCBOR()].AsString()
+        if (issuer != "AT") throw IllegalArgumentException("Issuer not correct: $issuer")
+        val issuedAt = Instant.ofEpochSecond(map[CwtHeaderKeys.ISSUED_AT.AsCBOR()].AsInt64())
+        if (issuedAt.isAfter(Instant.now())) throw IllegalArgumentException("IssuedAt not correct: $issuedAt")
+        val expirationTime = Instant.ofEpochSecond(map[CwtHeaderKeys.EXPIRATION.AsCBOR()].AsInt64())
+        if (expirationTime.isBefore(Instant.now())) throw IllegalArgumentException("Expiration not correct: $expirationTime")
+        val hcert = map[CwtHeaderKeys.HCERT.AsCBOR()]
+        return hcert[CBORObject.FromObject(1)].GetByteString()
     }
 
 }
