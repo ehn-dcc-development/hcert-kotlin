390b94ffe0f67e0563e22bf8b57066af2d0f1429
Florian Draschbacher <florian.draschbacher@iaik.tugraz.at>
2021-05-14 12:54:40 +0200

    Add zlib compression level and error handling in JS

--- src/jsMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/DefaultCompressorService.kt
+++ src/jsMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/DefaultCompressorService.kt
@@ -5,7 +5,7 @@ import ehn.techiop.hcert.kotlin.chain.VerificationResult
 import org.khronos.webgl.Uint8Array
 import org.khronos.webgl.get
 
-actual open class DefaultCompressorService(private val level: Int = 9) : CompressorService {
+actual open class DefaultCompressorService(private val compressionLevel: Int = 9) : CompressorService {
     private fun byteArrayToUint8Array(input: ByteArray): Uint8Array {
         return Uint8Array(input.toTypedArray())
     }
@@ -15,13 +15,25 @@ actual open class DefaultCompressorService(private val level: Int = 9) : Compres
     }
 
     override fun encode(input: ByteArray): ByteArray {
-        // TODO: Compression Level
-        return uint8ArrayToByteArray(Pako.deflate(byteArrayToUint8Array(input)))
+        return uint8ArrayToByteArray(
+            Pako.deflate(byteArrayToUint8Array(input),
+                object: Pako.DeflateFunctionOptions {
+                    override var level: dynamic
+                        get() = compressionLevel
+                        set(value) {}
+                }) as Uint8Array
+        )
     }
 
     override fun decode(input: ByteArray, verificationResult: VerificationResult): ByteArray {
-        // TODO: Compression Level, Verification Result
-        return uint8ArrayToByteArray(Pako.inflate(byteArrayToUint8Array(input)))
+        verificationResult.zlibDecoded = false
+        return try {
+            return uint8ArrayToByteArray(Pako.inflate(byteArrayToUint8Array(input))).also {
+                verificationResult.zlibDecoded = true
+            }
+        } catch (e: Throwable) {
+            input
+        }
     }
 
     actual companion object {
