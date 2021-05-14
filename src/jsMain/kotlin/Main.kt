1206faddc269c1ea3055286cdd14f4fec5e3761e
Florian Draschbacher <florian.draschbacher@iaik.tugraz.at>
2021-05-14 16:37:05 +0200

    Added experimental browserified cose

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -11,6 +11,7 @@ fun main(){
     println(Base45Encoder.encode(ByteArray(5){ i->i.toByte()}))
     val foo="Foo"
     val bar = js("externalTest(foo)")
+
     val cert = GreenCertificate("1.2", Person("Mustermann",null,"Max")
         , LocalDate(1980,6,8), listOf(Vaccination(ValueSetEntryAdapter("foo", ValueSetEntry("Voo","DE",true,"Soylent","ø")),ValueSetEntryAdapter("foo", ValueSetEntry("Voo","DE",true,"Soylent","ø")),ValueSetEntryAdapter("foo", ValueSetEntry("Voo","DE",true,"Soylent","ø")),ValueSetEntryAdapter("foo", ValueSetEntry("Voo","DE",true,"Soylent","ø")),9,100,
             LocalDate(2021,8,15),"AU","Royal Navy","HMCS"
@@ -19,9 +20,18 @@ fun main(){
     val input = DefaultCborService().encode(cert)
     val encode = Base45Encoder.encode(input)
     println(encode)
-    val enc=DefaultCompressorService().encode(input)
-    println(Base45Encoder.encode(enc))
+    val compressed = DefaultCompressorService().encode(input)
+    println(Base45Encoder.encode(compressed))
+    val decompressed = DefaultCompressorService().decode(compressed, VerificationResult())
+
+    println(input)
+    println(decompressed)
+    println("Match:" + input.contentEquals(decompressed))
+
     val ceert = DefaultCborService().decode(input, VerificationResult())
     println(Json.encodeToString(ceert))
     println(bar)
+
+    val cose = js("extrequire('cose-js')")
+    println(cose)
 }
