84b3a854534510e4194240fa8cc04c546c882480
Bernd Prünster <bernd.pruenster@iaik.tugraz.at>
2021-05-17 17:33:15 +0200

    Demo: Load QR code from file to avoid string escaping mistakes

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -12,12 +12,15 @@ import ehn.techiop.hcert.kotlin.crypto.Cose
 import ehn.techiop.hcert.kotlin.crypto.CoseJsEcPubKey
 import ehn.techiop.hcert.kotlin.crypto.CurveIdentifier
 import ehn.techiop.hcert.kotlin.data.*
+import kotlinx.browser.window
 import kotlinx.datetime.LocalDate
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.khronos.webgl.Uint8Array
 import org.khronos.webgl.get
 import pkijs.src.Certificate.Certificate
+import org.w3c.fetch.Request
+import org.w3c.xhr.XMLHttpRequest
 
 
 fun main() {
@@ -98,12 +101,19 @@ fun main() {
             "2opotWG8tJXN84ZZqT6wUBz9KF8D+z9NukYvnUEQ3QIgdBLFSTSiDt0UJaDF6St2\n" +
             "bkUQuVHW6fQbONd731/M4nc=\n" +
             "-----END CERTIFICATE-----\n"
-    val chain = DefaultChain.buildVerificationChain(PrefilledCertificateRepository(dscPem))
-    val qrCode = "HC1:NCFOXN%TS3DH3ZSUZK+.V0ETD%65NL-AH%TAIOOP-I*WA1778WAKQKMYSKD93B4:ZH6I1\$4JN:IN1MKK9+OC*PP:+P*.1D9R+Q6646C%6RF6:X93O5RF6\$T61R6B46646VY9WC5ME65H1KD34LT HBSZ4GH0B69X5Q/36MZ5BTMUW5-5QNF6O M9R1RF6ECM676746C0FFS6NWE0Y6Z EJZ6KS6YQEE%61Y6LMEA46B 17PPDFPVX1R270:6NEQ0R6AOM6PPL4Q0RUS-EBHU68E1W5XC52T9UF5LDCPF5RBQ746B46O1N646BQ99Q9E\$B ZJY1B QTOD3CQSP\$SR\$S3NDC9UOD3Y.TJET9G3DZI65BDM14NJ*XI-XIFRLL:F*ZR.OVOIF/HLTNP8EFNC3P:HDD8B1MM1M9NTNC30GH.Z8VHL+KLF%CD 810H% 0R%0ZD5CC9T0H\$/I*44SA3ZX8AT5XVMZGU\$X9+-5SL91PR%F6PH4R8Q0\$9UB9VXM0OPMR1A*CS:3.+4J0V/ED.EL5OR-AV.EJ4Z8WDRM V06PW1LRED0RLSZ18BN0BWF10*4Q50 "
-    val verificationResult = VerificationResult()
-    val greenCertificate = chain.decode(qrCode, verificationResult)
-    console.info(verificationResult)
-    console.info(greenCertificate)
+    val dscHex= "30 82 01 bd 30 82 01 63 a0 03 02 01 02 02 0a 01 79 3c 8b cf 0e 95 e2 ec b9 30 0a 06 08 2a 86 48 ce 3d 04 03 02 30 36 31 16 30 14 06 03 55 04 03 0c 0d 41 54 20 44 47 43 20 43 53 43 41 20 31 31 0b 30 09 06 03 55 04 06 13 02 41 54 31 0f 30 0d 06 03 55 04 0a 0c 06 42 4d 53 47 50 4b 30 1e 17 0d 32 31 30 35 30 35 31 32 34 31 30 36 5a 17 0d 32 33 30 35 30 35 31 32 34 31 30 36 5a 30 3d 31 11 30 0f 06 03 55 04 03 0c 08 41 54 20 44 53 43 20 31 31 0b 30 09 06 03 55 04 06 13 02 41 54 31 0f 30 0d 06 03 55 04 0a 0c 06 42 4d 53 47 50 4b 31 0a 30 08 06 03 55 04 05 13 01 31 30 59 30 13 06 07 2a 86 48 ce 3d 02 01 06 08 2a 86 48 ce 3d 03 01 07 03 42 00 04 ad d5 5c f5 ad 1b 96 d4 7a 8e 6d 41 3d 30 37 bb 47 32 24 d6 0a b8 5d 6e 46 4f 21 ee 1d 38 f9 70 51 27 d9 18 1e df bf a1 20 d7 c2 65 97 28 ce 9c 10 29 dc 9a a6 8a cf 50 fd 53 13 b5 16 97 41 77 a3 52 30 50 30 0e 06 03 55 1d 0f 01 01 ff 04 04 03 02 07 80 30 1d 06 03 55 1d 0e 04 16 04 14 ee 3d cb 52 12 6f a7 2f 5e c8 4b 72 9f 14 52 9f 55 a0 62 d0 30 1f 06 03 55 1d 23 04 18 30 16 80 14 fe c9 28 43 9f 94 61 2f 79 fd a2 31 db 68 a6 c7 ad c0 61 9e 30 0a 06 08 2a 86 48 ce 3d 04 03 02 03 48 00 30 45 02 21 00 c6 da 8a 68 b5 61 bc b4 95 cd f3 86 59 a9 3e b0 50 1c fd 28 5f 03 fb 3f 4d ba 46 2f 9d 41 10 dd 02 20 74 12 c5 49 34 a2 0e dd 14 25 a0 c5 e9 2b 76 6e 45 10 b9 51 d6 e9 f4 1b 38 d7 7b df 5f cc e2 77".split(' ').joinToString(separator = "").fromHexString()
+    val jsCertificate = JsCertificate(dscHex)
+    console.info(jsCertificate)
+    val chain = DefaultChain.buildVerificationChain(PrefilledCertificateRepository(jsCertificate))
+    window.fetch(Request("qr.txt")).then(onFulfilled = {
+        it.text().then(onFulfilled = {
+            println("GOT QR Code: $it")
+            val qrCode = it.trimIndent().trim()
+            val verificationResult = VerificationResult()
+            val greenCertificate = chain.decode(qrCode, verificationResult)
+            console.info(verificationResult)
+            console.info(greenCertificate)
+        })})
 
 }
 
