ed033c58819e16a99e21387a3aace40d3877ef29
Christian Kollmann <christian.kollmann@a-sit.at>
2021-06-10 10:17:51 +0200

    Pull PrefilledCertificateRepository into common code

--- src/commonMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/PrefilledCertificateRepository.kt
+++ src/commonMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/PrefilledCertificateRepository.kt
@@ -1,13 +1,37 @@
 package ehn.techiop.hcert.kotlin.chain.impl
 
 import ehn.techiop.hcert.kotlin.chain.CertificateRepository
+import ehn.techiop.hcert.kotlin.chain.Error
+import ehn.techiop.hcert.kotlin.chain.VerificationResult
 import ehn.techiop.hcert.kotlin.crypto.CertificateAdapter
+import ehn.techiop.hcert.kotlin.trust.TrustedCertificate
 
-expect class PrefilledCertificateRepository : CertificateRepository {
+class PrefilledCertificateRepository : CertificateRepository {
 
-    constructor(pemEncoded: String)
+    private val list = mutableListOf<CertificateAdapter>()
 
-    constructor(vararg certificates: CertificateAdapter)
+    constructor(vararg certificates: CertificateAdapter) {
+        certificates.forEach { list += it }
+    }
+
+    constructor(vararg pemEncodedCertificates: String) {
+        pemEncodedCertificates.forEach { list += CertificateAdapter(it) }
+    }
+
+    constructor(pemEncoded: String) {
+        list += CertificateAdapter(pemEncoded)
+    }
+
+    override fun loadTrustedCertificates(
+        kid: ByteArray,
+        verificationResult: VerificationResult
+    ): List<TrustedCertificate> {
+        val certList = list.filter { it.kid contentEquals kid }
+        if (certList.isEmpty()) throw IllegalArgumentException("kid").also {
+            verificationResult.error = Error.KEY_NOT_IN_TRUST_LIST
+        }
+        return certList.map { it.toTrustedCertificate() }
+    }
 
 }
 
