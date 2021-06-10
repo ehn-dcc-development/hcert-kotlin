25217d98a88f2ae8a5635bc2e76ec2bded57cd1e
Christian Kollmann <christian.kollmann@a-sit.at>
2021-06-10 07:28:13 +0200

    Extract certificate interface into separate file

--- src/commonMain/kotlin/ehn/techiop/hcert/kotlin/crypto/Datatypes.kt
+++ src/commonMain/kotlin/ehn/techiop/hcert/kotlin/crypto/Datatypes.kt
@@ -1,32 +1,5 @@
 package ehn.techiop.hcert.kotlin.crypto
 
-import ehn.techiop.hcert.kotlin.trust.ContentType
-import ehn.techiop.hcert.kotlin.trust.TrustedCertificateV2
-import kotlinx.datetime.Instant
-
-
-interface Certificate<T> {
-    val validContentTypes: List<ContentType>
-    val validFrom: Instant
-    val validUntil: Instant
-    val publicKey: PubKey<*>
-    fun toTrustedCertificate(): TrustedCertificateV2
-    val kid: ByteArray
-    val encoded: ByteArray
-}
-
-interface PrivKey<T> {
-    fun toCoseRepresentation(): T
-}
-
-interface EcPrivKey<T> : PrivKey<T>
-interface RsaPrivKey<T> : PrivKey<T>
-
-interface PubKey<T> {
-    fun toCoseRepresentation(): T
-}
-
-interface EcPubKey<T> : PubKey<T>
 
 enum class CoseHeaderKeys(val intVal: Int, val stringVal: String) {
     ALGORITHM(1, "alg"),
