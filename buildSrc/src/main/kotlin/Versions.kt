775b458fc031c0b3f8a9ad7722e8d78fef1e0123
Bernd Prünster <bernd.pruenster@iaik.tugraz.at>
2021-06-11 13:48:09 +0200

    externalise dependency versions

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,39 @@
+object Versions {
+    const val kotlin = "1.5.10"
+    const val serialization = "1.2.1"
+    const val datetime = "0.2.1"
+    const val kotest = "4.6.0"
+    const val licenseReport = "1.16"
+    val jvm = Jvm
+    val js = Js
+
+    object Jvm {
+        const val cose = "1.1.0"
+        const val zxing = "3.4.1"
+        const val bcpkix = "1.68"
+        const val validation = "2.0.1.Final"
+        const val jsonSchema = "0.20"
+    }
+
+    object Js {
+        const val pako = "2.0.3"
+        const val pakoTypes = "1.0.1"
+        const val pkijs = "2.1.95"
+        const val `crypto-browserify` = "3.12.0"
+        const val `stream-browserify` = "3.0.0"
+        const val `constants-browserify` = "1.0.0"
+        const val util = "0.12.3"
+        const val buffer = "6.0.3"
+        const val process = "0.11.10"
+        const val cbor = "7.0.5"
+        const val `node-inspect-extract` = "1.0.7"
+        const val sha256 = "1.3.0"
+        const val url = "0.11.0"
+        const val elliptic = "6.5.4"
+        const val rsa = "1.1.1"
+        const val assert = "2.0.0"
+        const val ajv = "8.5.0"
+        const val `ajv-formats` = "2.1.0"
+        const val qrcode = "3.0.1"
+    }
+}
