2e017a8849327f5a25f939dc22b1304478632260
Christian Kollmann <christian.kollmann@a-sit.at>
2021-06-11 14:34:22 +0200

    Refactor JVM schema validation

--- src/jvmMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/DefaultSchemaValidationService.kt
+++ src/jvmMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/DefaultSchemaValidationService.kt
@@ -12,16 +12,19 @@ actual class DefaultSchemaValidationService : SchemaValidationService {
     override fun validate(cbor: ByteArray, verificationResult: VerificationResult) {
         try {
             val decoded = CBORObject.DecodeFromBytes(cbor)
-            val json = ByteArrayOutputStream().let { decoded.WriteJSONTo(it);it.toString() }
-
-            val schema =
-                JSONSchema.parser.parse(javaClass.classLoader.getResource("json/DCC.combined-schema.json").toURI())
-
-            val output = schema.validateBasic(json)
-
-            output.errors?.forEach {
-                println("${it.error} - ${it.instanceLocation}")
-                verificationResult.error = Error.CBOR_DESERIALIZATION_FAILED
+            val json = ByteArrayOutputStream().let {
+                decoded.WriteJSONTo(it)
+                it.toString()
+            }
+            val resource = javaClass.classLoader.getResource("json/DCC.combined-schema.json")
+                ?: throw IllegalArgumentException("Schema not found")
+            val schema = JSONSchema.parser.parse(resource.toURI())
+            val result = schema.validateBasic(json)
+            result.errors?.let {
+                if (it.isNotEmpty()) {
+                    //it.forEach { println("${it.error} - ${it.instanceLocation}") }
+                    throw Throwable("Data does not follow schema: ${result.errors}")
+                }
             }
         } catch (t: Throwable) {
             verificationResult.error = Error.CBOR_DESERIALIZATION_FAILED
