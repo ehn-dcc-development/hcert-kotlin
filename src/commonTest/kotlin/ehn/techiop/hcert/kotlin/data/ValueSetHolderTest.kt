fdeff476da6a384af8e29ee733aae580b1ca3928
Christian Kollmann <christian.kollmann@a-sit.at>
2021-06-21 14:05:58 +0200

    Work around value set keys containing whitespace

--- src/commonTest/kotlin/ehn/techiop/hcert/kotlin/data/ValueSetHolderTest.kt
+++ src/commonTest/kotlin/ehn/techiop/hcert/kotlin/data/ValueSetHolderTest.kt
@@ -15,6 +15,16 @@ class ValueSetHolderTest : StringSpec({
         found.valueSetEntry.system shouldBe "http://snomed.info/sct"
     }
 
+    "Access by Key with Whitespace" {
+        val found = ValueSetsInstanceHolder.INSTANCE.find(" 840539006 ")
+        found.key shouldBe "840539006"
+        found.valueSetEntry.display shouldBe "COVID-19"
+        found.valueSetEntry.lang shouldBe "en"
+        found.valueSetEntry.active shouldBe true
+        found.valueSetEntry.version shouldBe "http://snomed.info/sct/900000000000207008/version/20210131"
+        found.valueSetEntry.system shouldBe "http://snomed.info/sct"
+    }
+
     "Access by Category Key" {
         val found = ValueSetsInstanceHolder.INSTANCE.find("vaccines-covid-19-names", "EU/1/20/1528")
         found.key shouldBe "EU/1/20/1528"
@@ -23,6 +33,5 @@ class ValueSetHolderTest : StringSpec({
         found.valueSetEntry.active shouldBe true
         found.valueSetEntry.version shouldBe ""
         found.valueSetEntry.system shouldBe "https://ec.europa.eu/health/documents/community-register/html/"
-
     }
 })
