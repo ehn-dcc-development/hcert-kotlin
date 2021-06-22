e24e2761a2f4cb8756319e6cff37ac609ca5c685
Christian Kollmann <christian.kollmann@a-sit.at>
2021-06-22 11:57:23 +0200

    Refactor capitalization of companion members

--- src/jvmMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/DefaultSchemaValidationService.kt
+++ src/jvmMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/DefaultSchemaValidationService.kt
@@ -4,8 +4,7 @@ import ehn.techiop.hcert.kotlin.chain.Error
 import ehn.techiop.hcert.kotlin.chain.SchemaValidationService
 import ehn.techiop.hcert.kotlin.chain.VerificationException
 import ehn.techiop.hcert.kotlin.chain.VerificationResult
-import ehn.techiop.hcert.kotlin.chain.impl.SchemaLoader.Companion.BASE_SCHEMA_VERSION
-import ehn.techiop.hcert.kotlin.chain.impl.SchemaLoader.Companion.knownSchemaVersions
+import ehn.techiop.hcert.kotlin.chain.impl.SchemaLoader.Companion.BASE_VERSION
 import ehn.techiop.hcert.kotlin.data.CborObject
 import ehn.techiop.hcert.kotlin.data.GreenCertificate
 import ehn.techiop.hcert.kotlin.trust.JvmCwtAdapter
@@ -39,11 +38,10 @@ actual class DefaultSchemaValidationService : SchemaValidationService {
                 Error.CBOR_DESERIALIZATION_FAILED,
                 "No schema version specified!"
             )
-            val validator = schemaLoader.validators[versionString]
-                ?: throw VerificationException(
-                    Error.SCHEMA_VALIDATION_FAILED,
-                    "Schema version $versionString is not supported. Supported versions are ${knownSchemaVersions.contentToString()}"
-                )
+            val validator = schemaLoader.validators[versionString] ?: throw VerificationException(
+                Error.SCHEMA_VALIDATION_FAILED,
+                "Schema version $versionString is not supported."
+            )
 
             val result = validator.validateBasic(json)
             result.errors?.let { error ->
@@ -51,7 +49,7 @@ actual class DefaultSchemaValidationService : SchemaValidationService {
                     //fallback to 1.3.0, since certificates may only conform to this never schema, even though they declare otherwise
                     //this is OK, though, as long as the specified version is actually valid
                     if (versionString < "1.3.0") {
-                        val validator13 = schemaLoader.validators[BASE_SCHEMA_VERSION]!!
+                        val validator13 = schemaLoader.validators[BASE_VERSION]!!
                         val result13 = validator13.validateBasic(json)
                         result13.errors?.let { error13 ->
                             if (error13.isNotEmpty()) throw VerificationException(
