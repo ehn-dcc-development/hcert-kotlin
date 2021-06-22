a94bf1e44838061d0d7c7ce184913429c77e3214
Christian Kollmann <christian.kollmann@a-sit.at>
2021-06-22 12:54:41 +0200

    Add lenient parsing of date objects

---
+++ src/commonMain/kotlin/ehn/techiop/hcert/kotlin/data/LenientLocalDateParser.kt
@@ -0,0 +1,31 @@
+package ehn.techiop.hcert.kotlin.data
+
+import kotlinx.datetime.LocalDate
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+
+/**
+ * Some countries include a timestamp, e.g. "T00:00:00",
+ * where everybody else expectso YYYY-MM-DD only,
+ * so strip it, to be able to parse it.
+ */
+object LenientLocalDateParser : KSerializer<LocalDate> {
+
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("LocalDate", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): LocalDate {
+        val value = decoder.decodeString()
+        return LocalDate.parse(value.substringBefore("T"))
+    }
+
+    override fun serialize(encoder: Encoder, value: LocalDate) {
+        encoder.encodeString(value.toString())
+    }
+}
+
