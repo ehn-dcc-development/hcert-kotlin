79323cf0897d723b9bd0160245b5f6b6d47ffdbc
Christian Kollmann <christian.kollmann@a-sit.at>
2021-06-23 11:13:47 +0200

    Reject HCERT data with more than one (t,r,v) entry
    Even two vaccination entries should have never been allowed

--- src/commonMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/DefaultHigherOrderValidationService.kt
+++ src/commonMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/DefaultHigherOrderValidationService.kt
@@ -1,6 +1,7 @@
 package ehn.techiop.hcert.kotlin.chain.impl
 
 import ehn.techiop.hcert.kotlin.chain.Error
+import ehn.techiop.hcert.kotlin.chain.Error.SCHEMA_VALIDATION_FAILED
 import ehn.techiop.hcert.kotlin.chain.HigherOrderValidationService
 import ehn.techiop.hcert.kotlin.chain.VerificationException
 import ehn.techiop.hcert.kotlin.chain.VerificationResult
@@ -19,7 +20,7 @@ class DefaultHigherOrderValidationService : HigherOrderValidationService {
         val numberNonNullVaccinations = input.vaccinations?.filterNotNull()?.size ?: 0
         if (numberNonNullVaccinations > 0) {
             if (numberNonNullTests > 0)
-                throw VerificationException(Error.SCHEMA_VALIDATION_FAILED, "Vaccination and test entry found")
+                throw VerificationException(SCHEMA_VALIDATION_FAILED, "Vaccination and test entry found")
 
             verificationResult.content.add(ContentType.VACCINATION)
             if (!verificationResult.certificateValidContent.contains(ContentType.VACCINATION))
@@ -28,16 +29,19 @@ class DefaultHigherOrderValidationService : HigherOrderValidationService {
         val numberNonNullRecoveryStatements = input.recoveryStatements?.filterNotNull()?.size ?: 0
         if (numberNonNullRecoveryStatements > 0) {
             if (numberNonNullTests > 0)
-                throw VerificationException(Error.SCHEMA_VALIDATION_FAILED, "Recovery and test entry found")
+                throw VerificationException(SCHEMA_VALIDATION_FAILED, "Recovery and test entry found")
             if (numberNonNullVaccinations > 0)
-                throw VerificationException(Error.SCHEMA_VALIDATION_FAILED, "Recovery and vaccination entry found")
+                throw VerificationException(SCHEMA_VALIDATION_FAILED, "Recovery and vaccination entry found")
 
             verificationResult.content.add(ContentType.RECOVERY)
             if (!verificationResult.certificateValidContent.contains(ContentType.RECOVERY))
                 throw VerificationException(Error.UNSUITABLE_PUBLIC_KEY_TYPE, "Type Recovery not valid")
         }
         if (numberNonNullTests == 0 && numberNonNullVaccinations == 0 && numberNonNullRecoveryStatements == 0) {
-            throw VerificationException(Error.SCHEMA_VALIDATION_FAILED, "No test, vaccination, or recovery entry")
+            throw VerificationException(SCHEMA_VALIDATION_FAILED, "No test, vaccination, or recovery entry")
+        }
+        if (numberNonNullTests + numberNonNullVaccinations + numberNonNullRecoveryStatements > 1) {
+            throw VerificationException(SCHEMA_VALIDATION_FAILED, "More than one test, vaccination, and recovery entry")
         }
         return input
     }
