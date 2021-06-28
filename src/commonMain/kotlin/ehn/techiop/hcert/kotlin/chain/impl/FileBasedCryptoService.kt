ca5e8c24186f9bab786dd08669eb9edcf30e8b8b
Christian Kollmann <christian.kollmann@a-sit.at>
2021-06-28 11:40:21 +0200

    Pull up common code for FileBasedCryptoService

--- src/commonMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/FileBasedCryptoService.kt
+++ src/commonMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/FileBasedCryptoService.kt
@@ -1,6 +1,58 @@
 package ehn.techiop.hcert.kotlin.chain.impl
 
 import ehn.techiop.hcert.kotlin.chain.CryptoService
+import ehn.techiop.hcert.kotlin.chain.Error
+import ehn.techiop.hcert.kotlin.chain.VerificationException
+import ehn.techiop.hcert.kotlin.chain.VerificationResult
+import ehn.techiop.hcert.kotlin.chain.asBase64
+import ehn.techiop.hcert.kotlin.crypto.CertificateAdapter
+import ehn.techiop.hcert.kotlin.crypto.CoseHeaderKeys
+import ehn.techiop.hcert.kotlin.crypto.CwtAlgorithm
+import ehn.techiop.hcert.kotlin.crypto.PrivKey
+import ehn.techiop.hcert.kotlin.crypto.PubKey
 
-expect class FileBasedCryptoService constructor(pemEncodedPrivateKey: String, pemEncodedCertificate: String) :
-    CryptoService
+class FileBasedCryptoService constructor(pemEncodedPrivateKey: String, pemEncodedCertificate: String) :
+    CryptoService {
+
+    private val cryptoAdapter = LoadedCryptoAdapter(pemEncodedPrivateKey, pemEncodedCertificate)
+
+    override fun getCborHeaders() = listOf(
+        Pair(CoseHeaderKeys.ALGORITHM, cryptoAdapter.algorithmID),
+        Pair(CoseHeaderKeys.KID, cryptoAdapter.certificate.kid)
+    )
+
+    override fun getCborSigningKey() = cryptoAdapter.privateKey
+
+    override fun getCborVerificationKey(kid: ByteArray, verificationResult: VerificationResult): PubKey {
+        if (!(cryptoAdapter.certificate.kid contentEquals kid))
+            throw VerificationException(Error.KEY_NOT_IN_TRUST_LIST, "kid not known: $kid")
+
+        verificationResult.setCertificateData(cryptoAdapter.certificate)
+        return cryptoAdapter.certificate.publicKey
+    }
+
+    override fun getCertificate(): CertificateAdapter = cryptoAdapter.certificate
+
+    override fun exportPrivateKeyAsPem() = "-----BEGIN PRIVATE KEY-----\n" +
+            base64forPem(cryptoAdapter.exportPrivateKey) +
+            "\n-----END PRIVATE KEY-----\n"
+
+    override fun exportCertificateAsPem() = "-----BEGIN CERTIFICATE-----\n" +
+            base64forPem(cryptoAdapter.exportCertificate) +
+            "\n-----END CERTIFICATE-----\n"
+
+    private fun base64forPem(encoded: ByteArray) =
+        encoded.asBase64().chunked(64).joinToString(separator = "\n")
+
+}
+
+
+expect class LoadedCryptoAdapter constructor(pemEncodedPrivateKey: String, pemEncodedCertificate: String) {
+
+    val privateKey: PrivKey
+    val algorithmID: CwtAlgorithm
+    val certificate: CertificateAdapter
+    val exportPrivateKey: ByteArray
+    val exportCertificate: ByteArray
+
+}
--- src/jvmMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/FileBasedCryptoService.kt
+++ src/jvmMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/FileBasedCryptoService.kt
@@ -1,47 +1,37 @@
 package ehn.techiop.hcert.kotlin.chain.impl
 
-import ehn.techiop.hcert.kotlin.chain.CryptoService
-import ehn.techiop.hcert.kotlin.chain.Error
-import ehn.techiop.hcert.kotlin.chain.VerificationException
-import ehn.techiop.hcert.kotlin.chain.VerificationResult
 import ehn.techiop.hcert.kotlin.crypto.CertificateAdapter
-import ehn.techiop.hcert.kotlin.crypto.CoseHeaderKeys
 import ehn.techiop.hcert.kotlin.crypto.CwtAlgorithm
 import ehn.techiop.hcert.kotlin.crypto.JvmPrivKey
 import ehn.techiop.hcert.kotlin.crypto.JvmPubKey
+import ehn.techiop.hcert.kotlin.crypto.PrivKey
 import ehn.techiop.hcert.kotlin.crypto.PubKey
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.bouncycastle.openssl.PEMParser
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter
-import org.bouncycastle.openssl.jcajce.JcaPEMWriter
 import org.bouncycastle.openssl.jcajce.JcaPKCS8Generator
-import org.bouncycastle.util.io.pem.PemWriter
-import java.io.StringWriter
 import java.security.PrivateKey
-import java.security.PublicKey
 import java.security.Security
 import java.security.cert.CertificateFactory
 import java.security.cert.X509Certificate
 import java.security.interfaces.ECPrivateKey
 import java.security.interfaces.RSAPrivateKey
 
-actual class FileBasedCryptoService actual constructor(pemEncodedPrivateKey: String, pemEncodedCertificate: String) :
-    CryptoService {
+actual class LoadedCryptoAdapter actual constructor(pemEncodedPrivateKey: String, pemEncodedCertificate: String) {
 
-    private val privateKey: PrivateKey
-    private val publicKey: PublicKey
-    private val algorithmID: CwtAlgorithm
-    private val certificate: CertificateAdapter
-    private val keyId: ByteArray
+    private val jvmPrivKey: PrivateKey
+    actual val privateKey: PrivKey
+    actual val algorithmID: CwtAlgorithm
+    actual val certificate: CertificateAdapter
 
     init {
         Security.addProvider(BouncyCastleProvider())
         val read = PEMParser(pemEncodedPrivateKey.reader()).readObject() as PrivateKeyInfo
-        privateKey = JcaPEMKeyConverter().getPrivateKey(read)
-        algorithmID = when (privateKey) {
+        jvmPrivKey = JcaPEMKeyConverter().getPrivateKey(read)
+        algorithmID = when (jvmPrivKey) {
             is ECPrivateKey -> {
-                when (privateKey.params.curve.field.fieldSize) {
+                when (jvmPrivKey.params.curve.field.fieldSize) {
                     256 -> CwtAlgorithm.ECDSA_256
                     384 -> CwtAlgorithm.ECDSA_384
                     else -> throw IllegalArgumentException("KeyType unknown")
@@ -53,36 +43,11 @@ actual class FileBasedCryptoService actual constructor(pemEncodedPrivateKey: Str
         val x509Certificate = CertificateFactory.getInstance("X.509")
             .generateCertificate(pemEncodedCertificate.byteInputStream()) as X509Certificate
         certificate = CertificateAdapter(x509Certificate)
-        publicKey = x509Certificate.publicKey
-        keyId = certificate.kid
+        privateKey = JvmPrivKey(jvmPrivKey)
     }
 
-    override fun getCborHeaders() = listOf(
-        Pair(CoseHeaderKeys.ALGORITHM, algorithmID),
-        Pair(CoseHeaderKeys.KID, keyId)
-    )
-
-    override fun getCborSigningKey() = JvmPrivKey(privateKey)
-
-    override fun getCborVerificationKey(kid: ByteArray, verificationResult: VerificationResult): PubKey {
-        if (!(keyId contentEquals kid))
-            throw VerificationException(Error.KEY_NOT_IN_TRUST_LIST, "kid not known: $kid")
-
-        verificationResult.setCertificateData(certificate)
-        return JvmPubKey(publicKey)
-    }
-
-    override fun getCertificate(): CertificateAdapter = certificate
-
-    override fun exportPrivateKeyAsPem() = StringWriter().apply {
-        PemWriter(this).use {
-            it.writeObject(JcaPKCS8Generator(privateKey, null).generate())
-        }
-    }.toString()
-
-    override fun exportCertificateAsPem() = StringWriter().apply {
-        JcaPEMWriter(this).use { it.writeObject(certificate.certificate) }
-    }.toString()
+    actual val exportPrivateKey: ByteArray = JcaPKCS8Generator(jvmPrivKey, null).generate().content
+    actual val exportCertificate: ByteArray = certificate.encoded
 
 }
 
