6674e342ff68ce817e5b6a734d15f7813b4f417f
Christian Kollmann <christian.kollmann@a-sit.at>
2021-06-28 16:29:35 +0200

    Unify implementations of CryptoService

--- src/commonMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/FileBasedCryptoService.kt
+++ src/commonMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/FileBasedCryptoService.kt
@@ -1,46 +1,8 @@
 package ehn.techiop.hcert.kotlin.chain.impl
 
-import ehn.techiop.hcert.kotlin.chain.CryptoService
-import ehn.techiop.hcert.kotlin.chain.Error
-import ehn.techiop.hcert.kotlin.chain.VerificationException
-import ehn.techiop.hcert.kotlin.chain.VerificationResult
-import ehn.techiop.hcert.kotlin.chain.asBase64
-import ehn.techiop.hcert.kotlin.crypto.CertificateAdapter
-import ehn.techiop.hcert.kotlin.crypto.CoseHeaderKeys
 import ehn.techiop.hcert.kotlin.crypto.CryptoAdapter
-import ehn.techiop.hcert.kotlin.crypto.PubKey
 
-class FileBasedCryptoService constructor(pemEncodedPrivateKey: String, pemEncodedCertificate: String) :
-    CryptoService {
-
-    private val cryptoAdapter = CryptoAdapter(pemEncodedPrivateKey, pemEncodedCertificate)
-
-    override fun getCborHeaders() = listOf(
-        Pair(CoseHeaderKeys.ALGORITHM, cryptoAdapter.algorithm),
-        Pair(CoseHeaderKeys.KID, cryptoAdapter.certificate.kid)
-    )
-
-    override fun getCborSigningKey() = cryptoAdapter.privateKey
-
-    override fun getCborVerificationKey(kid: ByteArray, verificationResult: VerificationResult): PubKey {
-        if (!(cryptoAdapter.certificate.kid contentEquals kid))
-            throw VerificationException(Error.KEY_NOT_IN_TRUST_LIST, "kid not known: $kid")
-
-        verificationResult.setCertificateData(cryptoAdapter.certificate)
-        return cryptoAdapter.certificate.publicKey
-    }
-
-    override fun getCertificate(): CertificateAdapter = cryptoAdapter.certificate
-
-    override fun exportPrivateKeyAsPem() = "-----BEGIN PRIVATE KEY-----\n" +
-            base64forPem(cryptoAdapter.privateKeyEncoded) +
-            "\n-----END PRIVATE KEY-----\n"
-
-    override fun exportCertificateAsPem() = "-----BEGIN CERTIFICATE-----\n" +
-            base64forPem(cryptoAdapter.certificate.encoded) +
-            "\n-----END CERTIFICATE-----\n"
-
-    private fun base64forPem(encoded: ByteArray) =
-        encoded.asBase64().chunked(64).joinToString(separator = "\n")
-
-}
+class FileBasedCryptoService(
+    pemEncodedPrivateKey: String,
+    pemEncodedCertificate: String
+) : DefaultCryptoService(CryptoAdapter(pemEncodedPrivateKey, pemEncodedCertificate))
--- src/commonMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/RandomEcKeyCryptoService.kt
+++ src/commonMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/RandomEcKeyCryptoService.kt
@@ -1,52 +1,12 @@
 package ehn.techiop.hcert.kotlin.chain.impl
 
-import ehn.techiop.hcert.kotlin.chain.CryptoService
-import ehn.techiop.hcert.kotlin.chain.Error
-import ehn.techiop.hcert.kotlin.chain.VerificationException
-import ehn.techiop.hcert.kotlin.chain.VerificationResult
-import ehn.techiop.hcert.kotlin.chain.asBase64
-import ehn.techiop.hcert.kotlin.crypto.CertificateAdapter
-import ehn.techiop.hcert.kotlin.crypto.CoseHeaderKeys
 import ehn.techiop.hcert.kotlin.crypto.CryptoAdapter
 import ehn.techiop.hcert.kotlin.crypto.KeyType
-import ehn.techiop.hcert.kotlin.crypto.PubKey
 import ehn.techiop.hcert.kotlin.trust.ContentType
 import kotlinx.datetime.Clock
 
-class RandomEcKeyCryptoService constructor(
+class RandomEcKeyCryptoService(
     keySize: Int = 256,
     contentType: List<ContentType> = ContentType.values().toList(),
     clock: Clock = Clock.System
-) : CryptoService {
-
-    private val cryptoAdapter = CryptoAdapter(KeyType.EC, keySize, contentType, clock)
-
-    override fun getCborHeaders() = listOf(
-        Pair(CoseHeaderKeys.ALGORITHM, cryptoAdapter.algorithm),
-        Pair(CoseHeaderKeys.KID, cryptoAdapter.certificate.kid)
-    )
-
-    override fun getCborSigningKey() = cryptoAdapter.privateKey
-
-    override fun getCborVerificationKey(kid: ByteArray, verificationResult: VerificationResult): PubKey {
-        if (!(cryptoAdapter.certificate.kid contentEquals kid))
-            throw VerificationException(Error.KEY_NOT_IN_TRUST_LIST, "kid not known: $kid")
-
-        verificationResult.setCertificateData(cryptoAdapter.certificate)
-        return cryptoAdapter.certificate.publicKey
-    }
-
-    override fun getCertificate(): CertificateAdapter = cryptoAdapter.certificate
-
-    override fun exportPrivateKeyAsPem() = "-----BEGIN PRIVATE KEY-----\n" +
-            base64forPem(cryptoAdapter.privateKeyEncoded) +
-            "\n-----END PRIVATE KEY-----\n"
-
-    override fun exportCertificateAsPem() = "-----BEGIN CERTIFICATE-----\n" +
-            base64forPem(cryptoAdapter.certificate.encoded) +
-            "\n-----END CERTIFICATE-----\n"
-
-    private fun base64forPem(encoded: ByteArray) =
-        encoded.asBase64().chunked(64).joinToString(separator = "\n")
-
-}
+) : DefaultCryptoService(CryptoAdapter(KeyType.EC, keySize, contentType, clock))
